5518a20090451cd0b22817d296aacced1380e802
Fernando Brino <fernandobrino11@gmail.com>
2023-09-10 20:25:39 -0300

    feat: create flatlist groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+
 import { Header } from '@components/Header';
-import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { Container } from './styles';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+
   return (
     <Container>
       <Header />
@@ -11,7 +16,17 @@ export function Groups() {
         title='Turmas'
         subtitle='jogue com a sua turma'
       />
-      <GroupCard title='Ignite Group'/>
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+            <GroupCard 
+              title={item} 
+            />
+          )
+        }
+      />
+      
     </Container>
   );
 }
