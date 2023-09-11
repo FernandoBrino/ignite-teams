920f670e6885d630222d099917b11aa42f11bbb8
Fernando Brino <fernandobrino11@gmail.com>
2023-09-10 21:56:10 -0300

    feat: button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -33,7 +34,9 @@ export function Groups() {
           />
         )}
       />
-      
+      <Button 
+        title='Criar nova turma'
+      />
     </Container>
   );
 }
